3d4a53f4ed0440aecb9563d0ad72c56ea688fce0
20 <64438648@qq.com>
2017-07-15 17:48:20 +0800

    删除帐号密码
    忘记删自己的密码了

--- gifupload.py
+++ gifupload.py
@@ -5,8 +5,8 @@ header = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
 }
 logindata = {
-    'username':"[]",  #用户名
-    'password':"zsl2234"   #密码
+    'username':"",  #用户名
+    'password':""   #密码
 }
 sign = {
     'sign':"0"
@@ -25,5 +25,4 @@ s.post("http://www.acfun.cn/login.aspx",data = logindata,headers = header)
 #上传头像
 s2 = s.post("http://www.acfun.cn/member/upload_image.aspx",files = files,headers = header)
 print(s2.text)
-if(s2.text.find('SUCCESS',10,50) > 0):
-	print("修改成功")
+if(s2.text.find('SUCCES
